8614cdca658f7dafb7084f1276c5ddc39c5865bf
silkair <silkair001@naver.com>
2024-07-08 20:31:46 +0900

    feat: 이미지 조회, 삭제 API
    feat: 이미지 조회, 삭제 API 구현 + DB 휘발성 수정

--- image/urls.py
+++ image/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import upload_image
+from .views import upload_image, get_image, delete_image
 
 urlpatterns = [
-    path('api/v1/images/', upload_image, name='upload-image'),
+    path('api/v1/images/', upload_image, name='upload-image'),  # 이미지 업로드 엔드포인트
+    path('api/v1/images/<int:image_id>/', get_image, name='get-image'),  # 이미지 조회 엔드포인트
+    path('api/v1/images/<int:image_id>/delete/', delete_image, name='delete-image'),  # 이미지 삭제 엔드포인트
 ]
