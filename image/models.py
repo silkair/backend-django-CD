42d1e86ad17ce751bbb001deebcdd799a20b91f1
silkair <silkair001@naver.com>
2024-07-11 17:58:27 +0900

    feat: Celery & RabbitMQ 연동 및 이미지 업로드 기능 비동기 처리
    + 이미지 업로드 swagger 설정 변경

--- image/models.py
+++ image/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from user.models import User
 
 class Image(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1 ) #만약 오류뜨면 -> , default=1 설정 넣어줘서 입력. 처음에는 사용자가 없어서 오류남
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #만약 오류뜨면 -> , default=1 설정 넣어줘서 입력. 처음에는 사용자가 없어서 오류남
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
---
+++ image/tasks.py
@@ -0,0 +1,43 @@
+from celery import shared_task
+import boto3
+import uuid
+import base64
+from django.conf import settings
+import logging
+from io import BytesIO
+from .models import Image
+
+logger = logging.getLogger(__name__)
+
+@shared_task
+def upload_image_to_s3(file_name, file_content, content_type, image_id):
+    logger.info(f"Started uploading {file_name} to S3")
+    s3 = boto3.client('s3', region_name=settings.AWS_S3_REGION_NAME)
+    unique_filename = f"{uuid.uuid4()}_{file_name}"
+    file_content = base64.b64decode(file_content)  # base64로 인코딩된 파일 내용을 디코딩
+    file_obj = BytesIO(file_content)  # 파일 내용을 BytesIO 객체로 변환
+
+    # S3에 파일 업로드
+    s3.upload_fileobj(
+        file_obj,
+        settings.AWS_STORAGE_BUCKET_NAME,
+        unique_filename,
+        ExtraArgs={
+            "ContentType": content_type
+        }
+    )
+
+    # 업로드된 파일의 URL 생성
+    file_url = f"https://{settings.AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{unique_filename}"
+    logger.info(f"Finished uploading {file_name} to S3, URL: {file_url}")
+
+    # 데이터베이스 업데이트
+    try:
+        image_instance = Image.objects.get(id=image_id)
+        image_instance.image_url = file_url
+        image_instance.save()
+        logger.info(f"Updated Image {image_id} with URL: {file_url}")
+    except Image.DoesNotExist:
+        logger.error(f"Image with id {image_id} does not exist")
+
+    return file_url
