93cc227f29ecd85bff31b8b4d26cb08d8a3f3626
chaeeun1103 <chaeeun1103@inu.ac.kr>
2024-07-15 22:43:31 +0900

    feat: Test용

--- image_resizing/urls.py
+++ image_resizing/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import resize_background_image_view, resize_recreated_background_image_view, recreated_background_image_manage, background_image_manage
 
 urlpatterns = [
-    path('api/v1/resize-background-image', resize_background_image_view, name='resize-background-image'),
-    path('api/v1/resize-recreated-background-image', resize_recreated_background_image_view, name='resize-recreated-background-image'),
-    path('api/v1/resize-background-image/<int:background_image_id>', background_image_manage, name='background-image-manage'),
-    path('api/v1/resize-recreated-background-image/<int:recreated_background_image_id>', recreated_background_image_manage, name='recreated-background-image-manage'),
+    path('api/v1/resizings/', resize_background_image_view, name='resize-background-image'),
+    path('api/v1/resizings-recreated/', resize_recreated_background_image_view, name='resize-recreated-background-image'),
+    path('api/v1/resizings/<int:resizing_id>', background_image_manage, name='background-image-manage'),
+    path('api/v1/resizings-recreted/<int:_resizing_recreated_id>', recreated_background_image_manage, name='recreated-background-image-manage'),
 ]
