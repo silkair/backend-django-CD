b291bded53a2e8383ee43a85953ebe8adbaa48ae
chaeeun1103 <chaeeun1103@inu.ac.kr>
2024-07-19 00:18:16 +0900

    url, banner

--- image_resizing/urls.py
+++ image_resizing/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import resize_background_image_view, resize_recreated_background_image_view, recreated_background_image_manage, background_image_manage
 
 urlpatterns = [
-    path('api/v1/resizings/', resize_background_image_view, name='resize-background-image'),
-    path('api/v1/resizings-recreated/', resize_recreated_background_image_view, name='resize-recreated-background-image'),
-    path('api/v1/resizings/<int:resizing_id>', background_image_manage, name='background-image-manage'),
-    path('api/v1/resizings-recreted/<int:_resizing_recreated_id>', recreated_background_image_manage, name='recreated-background-image-manage'),
+    path('resizings/', resize_background_image_view, name='resize-background-image'),
+    path('resizings-recreated/', resize_recreated_background_image_view, name='resize-recreated-background-image'),
+    path('resizings/<int:resizing_id>', background_image_manage, name='background-image-manage'),
+    path('resizings-recreted/<int:resizing_recreated_id>', recreated_background_image_manage, name='recreated-background-image-manage'),
 ]
--- recreated_background/urls.py
+++ recreated_background/urls.py
@@ -3,7 +3,7 @@ from .views import recreate_background_view, recreated_background_manage
 
 
 urlpatterns = [
-    path('api/v1/recreated-backgrounds', recreate_background_view, name='recreated-backgrounds'),
-    path('api/v1/recreated-backgrounds/<int:recreated_background_id>', recreated_background_manage, name='recreated-background-manage')
+    path('recreated-backgrounds/', recreate_background_view, name='recreated-backgrounds'),
+    path('recreated-backgrounds/<int:recreated_background_id>', recreated_background_manage, name='recreated-background-manage')
 ]
 
