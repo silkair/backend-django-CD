f94ff056f4095a9a2aa6b5091d1fdbcb83eac5fd
silkair <silkair001@naver.com>
2024-07-20 03:21:09 +0900

    refactor: nginx https 수정
    수정수정

--- backend/settings.py
+++ backend/settings.py
@@ -140,6 +140,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Whitenoise 설정
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
