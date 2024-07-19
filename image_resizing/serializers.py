4373cc803b9905deb85d0282abaa83c9d110bd74
chaeeun1103 <chaeeun1103@inu.ac.kr>
2024-07-19 23:49:58 +0900

    imageresizing

--- image_resizing/serializers.py
+++ image_resizing/serializers.py
@@ -3,10 +3,12 @@ from .models import ImageResizing
 
 class BackgroundImageResizingSerializer(serializers.ModelSerializer):
     background_id = serializers.IntegerField()
+    width = serializers.IntegerField()
+    height = serializers.IntegerField()
 
     class Meta:
         model = ImageResizing
-        fields = ['background_id', 'image_url']
+        fields = ['background_id', 'width', 'height', 'image_url']
         read_only_fields = ['image_url', 'created_at', 'updated_at']
 
     def to_representation(self, instance):
@@ -16,11 +18,12 @@ class BackgroundImageResizingSerializer(serializers.ModelSerializer):
 
 class RecreatedBackgroundImageResizingSerializer(serializers.ModelSerializer):
     recreated_background_id = serializers.IntegerField()
+    width = serializers.IntegerField()
+    height = serializers.IntegerField()
 
     class Meta:
-
         model = ImageResizing
-        fields = ['recreated_background_id', 'image_url']
+        fields = ['recreated_background_id', 'width', 'height', 'image_url']
         read_only_fields = ['image_url', 'created_at', 'updated_at']
 
     def to_representation(self, instance):
